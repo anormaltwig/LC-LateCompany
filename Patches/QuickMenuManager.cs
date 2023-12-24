72bde88be25be4a3993eef01e550db0830b73312
Jenson R <mcplayer1a3@gmail.com>
2023-12-24 15:59:07 +0000

    Disable joining whilst mid-loading

--- Patches/QuickMenuManager.cs
+++ Patches/QuickMenuManager.cs
@@ -3,19 +3,20 @@ using HarmonyLib;
 namespace LateCompany.Patches;
 
 [HarmonyPatch(typeof(QuickMenuManager), nameof(QuickMenuManager.DisableInviteFriendsButton))]
-[HarmonyWrapSafe]
-internal static class DisableInviteFriendsButton_Patch {
-	[HarmonyPrefix]
-	private static bool Prefix() { return false; }
+internal static class DisableInviteFriendsButton_Patch
+{
+    [HarmonyPrefix]
+    private static bool Prefix() { return false; }
 }
 
 [HarmonyPatch(typeof(QuickMenuManager), nameof(QuickMenuManager.InviteFriendsButton))]
-[HarmonyWrapSafe]
-internal static class InviteFriendsButton_Patch {
-	[HarmonyPrefix]
-	private static bool Prefix() {
-		if (Plugin.LobbyJoinable) GameNetworkManager.Instance.InviteFriendsUI();
+internal static class InviteFriendsButton_Patch
+{
+    [HarmonyPrefix]
+    private static bool Prefix()
+    {
+        if (Plugin.LobbyJoinable) GameNetworkManager.Instance.InviteFriendsUI();
 
-		return false;
-	}
+        return false;
+    }
 }
--- Patches/StartOfRound.cs
+++ Patches/StartOfRound.cs
@@ -59,34 +59,118 @@ internal class OnPlayerConnectedClientRpc_Patch {
 	}
 }
 
-[HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.OnPlayerDC))]
+[HarmonyPatch(typeof(StartOfRound), "OnPlayerConnectedClientRpc")]
 [HarmonyWrapSafe]
-internal class OnPlayerDC_Patch {
-	[HarmonyPostfix]
-	public static void Postfix() {
-		if (!StartOfRound.Instance.inShipPhase && Plugin.OnlyLateJoinInOrbit) return;
+internal static class OnPlayerConnectedClientRpc_Patch
+{
+    public static MethodInfo BeginSendClientRpc = typeof(RoundManager).GetMethod("__beginSendClientRpc", BindingFlags.NonPublic | BindingFlags.Instance);
+    public static MethodInfo EndSendClientRpc = typeof(RoundManager).GetMethod("__endSendClientRpc", BindingFlags.NonPublic | BindingFlags.Instance);
 
-		Plugin.SetLobbyJoinable(true);
-	}
+    // Best guess at getting new players to load into the map after the game starts.
+    [HarmonyPostfix]
+    private static void Postfix(ulong clientId, int connectedPlayers, ulong[] connectedPlayerIdsOrdered, int assignedPlayerObjectId, int serverMoneyAmount, int levelID, int profitQuota, int timeUntilDeadline, int quotaFulfilled, int randomSeed)
+    {
+        StartOfRound sor = StartOfRound.Instance;
+        PlayerControllerB ply = sor.allPlayerScripts[assignedPlayerObjectId];
+
+        if (sor.connectedPlayersAmount + 1 >= sor.allPlayerScripts.Length)
+        {
+            Plugin.SetLobbyJoinable(false);
+        }
+
+        // Make their player model visible.
+        ply.DisablePlayerModel(sor.allPlayerObjects[assignedPlayerObjectId], true, true);
+
+        if (sor.IsServer && !sor.inShipPhase)
+        {
+            RoundManager rm = RoundManager.Instance;
+
+            ClientRpcParams clientRpcParams = new()
+            {
+                Send = new ClientRpcSendParams()
+                {
+                    TargetClientIds = new List<ulong>() { clientId },
+                },
+            };
+
+            // Tell the new client to generate the level.
+            {
+                FastBufferWriter fastBufferWriter = (FastBufferWriter)BeginSendClientRpc.Invoke(rm, new object[] { 1193916134U, clientRpcParams, 0 });
+                BytePacker.WriteValueBitPacked(fastBufferWriter, StartOfRound.Instance.randomMapSeed);
+                BytePacker.WriteValueBitPacked(fastBufferWriter, StartOfRound.Instance.currentLevelID);
+                BytePacker.WriteValueBitPacked(fastBufferWriter, (short)rm.currentLevel.currentWeather);
+                EndSendClientRpc.Invoke(rm, new object[] { fastBufferWriter, 1193916134U, clientRpcParams, 0 });
+            }
+
+            // And also tell them that everyone is done generating it.
+            {
+                FastBufferWriter fastBufferWriter = (FastBufferWriter)BeginSendClientRpc.Invoke(rm, new object[] { 2729232387U, clientRpcParams, 0 });
+                EndSendClientRpc.Invoke(rm, new object[] { fastBufferWriter, 2729232387U, clientRpcParams, 0 });
+            }
+        }
+
+        sor.livingPlayers = sor.connectedPlayersAmount + 1;
+        for (int i = 0; i < sor.allPlayerScripts.Length; i++)
+        {
+            PlayerControllerB pcb = sor.allPlayerScripts[i];
+            if (pcb.isPlayerControlled && pcb.isPlayerDead) sor.livingPlayers--;
+        }
+    }
 }
 
-[HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.StartGame))]
+[HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.OnPlayerDC))]
 [HarmonyWrapSafe]
-internal class StartGame_Patch {
-	[HarmonyPrefix]
-	public static void Prefix() {
-		if (Plugin.OnlyLateJoinInOrbit)
-			Plugin.SetLobbyJoinable(false);
+internal static class OnPlayerDC_Patch {
+	[HarmonyPostfix]
+	private static void Postfix() {
+		if (StartOfRound.Instance.inShipPhase || (!Plugin.OnlyLateJoinInOrbit && StartOfRound.Instance.shipHasLanded)) {
+			Plugin.SetLobbyJoinable(true);
+		}
 	}
 }
 
 [HarmonyPatch(typeof(StartOfRound), "SetShipReadyToLand")]
-[HarmonyWrapSafe]
-internal class SetShipReadyToLand_Patch {
-	[HarmonyPrefix]
-	public static void Postfix() {
-		if (Plugin.OnlyLateJoinInOrbit && StartOfRound.Instance.connectedPlayersAmount + 1 < StartOfRound.Instance.allPlayerScripts.Length)
-			Plugin.SetLobbyJoinable(true);
-	}
+internal static class SetShipReadyToLand_Patch
+{
+    [HarmonyPostfix]
+    private static void Postfix()
+    {
+        if (Plugin.OnlyLateJoinInOrbit && StartOfRound.Instance.connectedPlayersAmount + 1 < StartOfRound.Instance.allPlayerScripts.Length)
+        {
+            Plugin.SetLobbyJoinable(true);
+        }
+    }
 }
 
+[HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.StartGame))]
+internal static class StartGame_Patch
+{
+    [HarmonyPrefix]
+    private static void Prefix()
+    {
+        Plugin.SetLobbyJoinable(false);
+    }
+}
+
+[HarmonyPatch(typeof(StartOfRound), "OnShipLandedMiscEvents")]
+internal static class OnShipLandedMiscEvents_Patch
+{
+    [HarmonyPostfix]
+    private static void Postfix()
+    {
+        if (!Plugin.OnlyLateJoinInOrbit && StartOfRound.Instance.connectedPlayersAmount + 1 < StartOfRound.Instance.allPlayerScripts.Length)
+        {
+            Plugin.SetLobbyJoinable(true);
+        }
+    }
+}
+
+[HarmonyPatch(typeof(StartOfRound), "ShipLeave")]
+internal static class ShipLeave_Patch
+{
+    [HarmonyPostfix]
+    private static void Postfix()
+    {
+        Plugin.SetLobbyJoinable(false);
+    }
+}
