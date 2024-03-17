a2bc9768483382d53554a2e8786f446e0e4f045b
1A3 <mcplayer1a3@gmail.com>
2024-03-17 20:34:18 +0000

    Fixed isPlayerControlled being set to true on dead players when someone joins mid-game

--- Patches/StartOfRound.cs
+++ Patches/StartOfRound.cs
@@ -5,28 +5,87 @@ using Unity.Netcode;
 using GameNetcodeStuff;
 
 using HarmonyLib;
+using System.Linq;
+using System.Reflection.Emit;
+using UnityEngine;
 
 namespace LateCompany.Patches;
 
 [HarmonyPatch(typeof(StartOfRound), "OnPlayerConnectedClientRpc")]
 [HarmonyWrapSafe]
 internal static class OnPlayerConnectedClientRpc_Patch {
-	public static MethodInfo BeginSendClientRpc = typeof(RoundManager).GetMethod("__beginSendClientRpc", BindingFlags.NonPublic | BindingFlags.Instance);
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var newInstructions = new List<CodeInstruction>();
+        bool foundInitial = false;
+        bool shouldSkip = false;
+        bool alreadyReplaced = false;
+
+        foreach (var instruction in instructions)
+        {
+            if (!alreadyReplaced)
+            {
+                if (!foundInitial && instruction.opcode == OpCodes.Call && instruction.operand != null && instruction.operand.ToString() == "System.Collections.IEnumerator setPlayerToSpawnPosition(UnityEngine.Transform, UnityEngine.Vector3)")
+                {
+                    foundInitial = true;
+                }
+                else if (foundInitial && instruction.opcode == OpCodes.Ldc_I4_0)
+                {
+                    shouldSkip = true;
+                    continue;
+                }
+                else if (shouldSkip && instruction.opcode == OpCodes.Ldloc_0)
+                {
+                    shouldSkip = false;
+                    alreadyReplaced = true;
+                }
+            }
+
+            if (!shouldSkip)
+            {
+                newInstructions.Add(instruction);
+            }
+        }
+
+        if (!alreadyReplaced) Debug.LogError("Failed to transpile StartOfRound::OnPlayerConnectedClientRpc");
+
+        return newInstructions.AsEnumerable();
+    }
+
+    public static MethodInfo BeginSendClientRpc = typeof(RoundManager).GetMethod("__beginSendClientRpc", BindingFlags.NonPublic | BindingFlags.Instance);
 	public static MethodInfo EndSendClientRpc = typeof(RoundManager).GetMethod("__endSendClientRpc", BindingFlags.NonPublic | BindingFlags.Instance);
 
 	// Best guess at getting new players to load into the map after the game starts.
 	[HarmonyPostfix]
-	private static void Postfix(ulong clientId, int connectedPlayers, ulong[] connectedPlayerIdsOrdered, int assignedPlayerObjectId, int serverMoneyAmount, int levelID, int profitQuota, int timeUntilDeadline, int quotaFulfilled, int randomSeed) {
-		StartOfRound sor = StartOfRound.Instance;
-		PlayerControllerB ply = sor.allPlayerScripts[assignedPlayerObjectId];
-
-		if (sor.connectedPlayersAmount + 1 >= sor.allPlayerScripts.Length)
+	private static void Postfix(StartOfRound __instance, ulong clientId, int connectedPlayers, ulong[] connectedPlayerIdsOrdered, int assignedPlayerObjectId, int serverMoneyAmount, int levelID, int profitQuota, int timeUntilDeadline, int quotaFulfilled, int randomSeed) {
+		if (__instance.connectedPlayersAmount + 1 >= __instance.allPlayerScripts.Length)
 			Plugin.SetLobbyJoinable(false);
 
-		// Make their player model visible.
-		ply.DisablePlayerModel(sor.allPlayerObjects[assignedPlayerObjectId], true, true);
+        PlayerControllerB ply = __instance.allPlayerScripts[assignedPlayerObjectId];
+        // Make their player model visible.
+        ply.DisablePlayerModel(__instance.allPlayerObjects[assignedPlayerObjectId], true, true);
 
-		if (sor.IsServer && !sor.inShipPhase) {
+        __instance.livingPlayers = __instance.connectedPlayersAmount + 1;
+        for (int i = 0; i < __instance.allPlayerScripts.Length; i++)
+        {
+            PlayerControllerB pcb = __instance.allPlayerScripts[i];
+            if (pcb.isPlayerControlled && pcb.isPlayerDead) __instance.livingPlayers--;
+        }
+
+        if (NetworkManager.Singleton != null)
+        {
+            for (int j = 0; j < __instance.connectedPlayersAmount + 1; j++)
+            {
+                // Don't set the player as controlled if they are dead.
+                if ((j == 0 || !__instance.allPlayerScripts[j].IsOwnedByServer) && !__instance.allPlayerScripts[j].isPlayerDead)
+                {
+                    __instance.allPlayerScripts[j].isPlayerControlled = true;
+                }
+            }
+        }
+
+        if (__instance.IsServer && !__instance.inShipPhase) {
 			RoundManager rm = RoundManager.Instance;
 
 			ClientRpcParams clientRpcParams = new() {
@@ -38,8 +97,8 @@ internal static class OnPlayerConnectedClientRpc_Patch {
 			// Tell the new client to generate the level.
 			{
 				FastBufferWriter fastBufferWriter = (FastBufferWriter)BeginSendClientRpc.Invoke(rm, new object[] { 1193916134U, clientRpcParams, 0 });
-				BytePacker.WriteValueBitPacked(fastBufferWriter, StartOfRound.Instance.randomMapSeed);
-				BytePacker.WriteValueBitPacked(fastBufferWriter, StartOfRound.Instance.currentLevelID);
+				BytePacker.WriteValueBitPacked(fastBufferWriter, __instance.randomMapSeed);
+				BytePacker.WriteValueBitPacked(fastBufferWriter, __instance.currentLevelID);
 				BytePacker.WriteValueBitPacked(fastBufferWriter, (int)rm.currentLevel.currentWeather + 0xFF);
 				EndSendClientRpc.Invoke(rm, new object[] { fastBufferWriter, 1193916134U, clientRpcParams, 0 });
 			}
@@ -50,13 +109,7 @@ internal static class OnPlayerConnectedClientRpc_Patch {
 				EndSendClientRpc.Invoke(rm, new object[] { fastBufferWriter, 2729232387U, clientRpcParams, 0 });
 			}
 		}
-
-		sor.livingPlayers = sor.connectedPlayersAmount + 1;
-		for (int i = 0; i < sor.allPlayerScripts.Length; i++) {
-			PlayerControllerB pcb = sor.allPlayerScripts[i];
-			if (pcb.isPlayerControlled && pcb.isPlayerDead) sor.livingPlayers--;
-		}
-	}
+    }
 }
 
 [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.OnPlayerDC))]
