fa51095a475a0d07a61b4ea084c993b8e2dcb7a1
1A3 <mcplayer1a3@gmail.com>
2024-03-17 20:47:28 +0000

    use tabs for spacing because vs is dumb

--- Patches/StartOfRound.cs
+++ Patches/StartOfRound.cs
@@ -14,102 +14,102 @@ namespace LateCompany.Patches;
 [HarmonyPatch(typeof(StartOfRound), "OnPlayerConnectedClientRpc")]
 [HarmonyWrapSafe]
 internal static class OnPlayerConnectedClientRpc_Patch {
-    [HarmonyTranspiler]
-    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+[HarmonyTranspiler]
+private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+{
+    var newInstructions = new List<CodeInstruction>();
+    bool foundInitial = false;
+    bool shouldSkip = false;
+    bool alreadyReplaced = false;
+
+    foreach (var instruction in instructions)
     {
-        var newInstructions = new List<CodeInstruction>();
-        bool foundInitial = false;
-        bool shouldSkip = false;
-        bool alreadyReplaced = false;
-
-        foreach (var instruction in instructions)
+        if (!alreadyReplaced)
         {
-            if (!alreadyReplaced)
+            if (!foundInitial && instruction.opcode == OpCodes.Call && instruction.operand != null && instruction.operand.ToString() == "System.Collections.IEnumerator setPlayerToSpawnPosition(UnityEngine.Transform, UnityEngine.Vector3)")
             {
-                if (!foundInitial && instruction.opcode == OpCodes.Call && instruction.operand != null && instruction.operand.ToString() == "System.Collections.IEnumerator setPlayerToSpawnPosition(UnityEngine.Transform, UnityEngine.Vector3)")
-                {
-                    foundInitial = true;
-                }
-                else if (foundInitial && instruction.opcode == OpCodes.Ldc_I4_0)
-                {
-                    shouldSkip = true;
-                    continue;
-                }
-                else if (shouldSkip && instruction.opcode == OpCodes.Ldloc_0)
-                {
-                    shouldSkip = false;
-                    alreadyReplaced = true;
-                }
+                foundInitial = true;
             }
-
-            if (!shouldSkip)
+            else if (foundInitial && instruction.opcode == OpCodes.Ldc_I4_0)
             {
-                newInstructions.Add(instruction);
+                shouldSkip = true;
+                continue;
+            }
+            else if (shouldSkip && instruction.opcode == OpCodes.Ldloc_0)
+            {
+                shouldSkip = false;
+                alreadyReplaced = true;
             }
         }
 
-        if (!alreadyReplaced) Debug.LogError("Failed to transpile StartOfRound::OnPlayerConnectedClientRpc");
-
-        return newInstructions.AsEnumerable();
+        if (!shouldSkip)
+        {
+            newInstructions.Add(instruction);
+        }
     }
 
-    public static MethodInfo BeginSendClientRpc = typeof(RoundManager).GetMethod("__beginSendClientRpc", BindingFlags.NonPublic | BindingFlags.Instance);
-    public static MethodInfo EndSendClientRpc = typeof(RoundManager).GetMethod("__endSendClientRpc", BindingFlags.NonPublic | BindingFlags.Instance);
+    if (!alreadyReplaced) Debug.LogError("Failed to transpile StartOfRound::OnPlayerConnectedClientRpc");
 
-    // Best guess at getting new players to load into the map after the game starts.
-    [HarmonyPostfix]
-    private static void Postfix(StartOfRound __instance, ulong clientId, int connectedPlayers, ulong[] connectedPlayerIdsOrdered, int assignedPlayerObjectId, int serverMoneyAmount, int levelID, int profitQuota, int timeUntilDeadline, int quotaFulfilled, int randomSeed) {
-	    if (__instance.connectedPlayersAmount + 1 >= __instance.allPlayerScripts.Length)
-		    Plugin.SetLobbyJoinable(false);
+    return newInstructions.AsEnumerable();
+}
 
-        PlayerControllerB ply = __instance.allPlayerScripts[assignedPlayerObjectId];
-        // Make their player model visible.
-        ply.DisablePlayerModel(__instance.allPlayerObjects[assignedPlayerObjectId], true, true);
+public static MethodInfo BeginSendClientRpc = typeof(RoundManager).GetMethod("__beginSendClientRpc", BindingFlags.NonPublic | BindingFlags.Instance);
+public static MethodInfo EndSendClientRpc = typeof(RoundManager).GetMethod("__endSendClientRpc", BindingFlags.NonPublic | BindingFlags.Instance);
 
-        __instance.livingPlayers = __instance.connectedPlayersAmount + 1;
-        for (int i = 0; i < __instance.allPlayerScripts.Length; i++)
+// Best guess at getting new players to load into the map after the game starts.
+[HarmonyPostfix]
+private static void Postfix(StartOfRound __instance, ulong clientId, int connectedPlayers, ulong[] connectedPlayerIdsOrdered, int assignedPlayerObjectId, int serverMoneyAmount, int levelID, int profitQuota, int timeUntilDeadline, int quotaFulfilled, int randomSeed) {
+	if (__instance.connectedPlayersAmount + 1 >= __instance.allPlayerScripts.Length)
+		Plugin.SetLobbyJoinable(false);
+
+    PlayerControllerB ply = __instance.allPlayerScripts[assignedPlayerObjectId];
+    // Make their player model visible.
+    ply.DisablePlayerModel(__instance.allPlayerObjects[assignedPlayerObjectId], true, true);
+
+    __instance.livingPlayers = __instance.connectedPlayersAmount + 1;
+    for (int i = 0; i < __instance.allPlayerScripts.Length; i++)
+    {
+        PlayerControllerB pcb = __instance.allPlayerScripts[i];
+        if (pcb.isPlayerControlled && pcb.isPlayerDead) __instance.livingPlayers--;
+    }
+
+    if (NetworkManager.Singleton != null)
+    {
+        for (int j = 0; j < __instance.connectedPlayersAmount + 1; j++)
         {
-            PlayerControllerB pcb = __instance.allPlayerScripts[i];
-            if (pcb.isPlayerControlled && pcb.isPlayerDead) __instance.livingPlayers--;
-        }
-
-        if (NetworkManager.Singleton != null)
-        {
-            for (int j = 0; j < __instance.connectedPlayersAmount + 1; j++)
+            // Don't set the player as controlled if they are dead.
+            if ((j == 0 || !__instance.allPlayerScripts[j].IsOwnedByServer) && !__instance.allPlayerScripts[j].isPlayerDead)
             {
-                // Don't set the player as controlled if they are dead.
-                if ((j == 0 || !__instance.allPlayerScripts[j].IsOwnedByServer) && !__instance.allPlayerScripts[j].isPlayerDead)
-                {
-                    __instance.allPlayerScripts[j].isPlayerControlled = true;
-                }
+                __instance.allPlayerScripts[j].isPlayerControlled = true;
             }
         }
-
-        if (__instance.IsServer && !__instance.inShipPhase) {
-		    RoundManager rm = RoundManager.Instance;
-
-		    ClientRpcParams clientRpcParams = new() {
-			    Send = new ClientRpcSendParams() {
-				    TargetClientIds = new List<ulong>() { clientId },
-			    },
-		    };
-
-		    // Tell the new client to generate the level.
-		    {
-			    FastBufferWriter fastBufferWriter = (FastBufferWriter)BeginSendClientRpc.Invoke(rm, new object[] { 1193916134U, clientRpcParams, 0 });
-			    BytePacker.WriteValueBitPacked(fastBufferWriter, __instance.randomMapSeed);
-			    BytePacker.WriteValueBitPacked(fastBufferWriter, __instance.currentLevelID);
-			    BytePacker.WriteValueBitPacked(fastBufferWriter, (int)rm.currentLevel.currentWeather + 0xFF);
-			    EndSendClientRpc.Invoke(rm, new object[] { fastBufferWriter, 1193916134U, clientRpcParams, 0 });
-		    }
-
-		    // And also tell them that everyone is done generating it.
-		    {
-			    FastBufferWriter fastBufferWriter = (FastBufferWriter)BeginSendClientRpc.Invoke(rm, new object[] { 2729232387U, clientRpcParams, 0 });
-			    EndSendClientRpc.Invoke(rm, new object[] { fastBufferWriter, 2729232387U, clientRpcParams, 0 });
-		    }
-	    }
     }
+
+    if (__instance.IsServer && !__instance.inShipPhase) {
+		RoundManager rm = RoundManager.Instance;
+
+		ClientRpcParams clientRpcParams = new() {
+			Send = new ClientRpcSendParams() {
+				TargetClientIds = new List<ulong>() { clientId },
+			},
+		};
+
+		// Tell the new client to generate the level.
+		{
+			FastBufferWriter fastBufferWriter = (FastBufferWriter)BeginSendClientRpc.Invoke(rm, new object[] { 1193916134U, clientRpcParams, 0 });
+			BytePacker.WriteValueBitPacked(fastBufferWriter, __instance.randomMapSeed);
+			BytePacker.WriteValueBitPacked(fastBufferWriter, __instance.currentLevelID);
+			BytePacker.WriteValueBitPacked(fastBufferWriter, (int)rm.currentLevel.currentWeather + 0xFF);
+			EndSendClientRpc.Invoke(rm, new object[] { fastBufferWriter, 1193916134U, clientRpcParams, 0 });
+		}
+
+		// And also tell them that everyone is done generating it.
+		{
+			FastBufferWriter fastBufferWriter = (FastBufferWriter)BeginSendClientRpc.Invoke(rm, new object[] { 2729232387U, clientRpcParams, 0 });
+			EndSendClientRpc.Invoke(rm, new object[] { fastBufferWriter, 2729232387U, clientRpcParams, 0 });
+		}
+	}
+}
 }
 
 [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.OnPlayerDC))]
